0b5ed0c70c4e17d8a2c1bb5826af39aee04dea38
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-11-08 20:25:53 +0530

    -kind of broken code

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,28 @@ import Home from './components/Home'
 import Notes from './components/Notes'
 import Users from './components/Users'
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { getNotes } from './requests'
 const App = () => {
+  const [notes, setNotes] = useState([{content:'hello',important:'',id:''}])
   const padding = {
     padding: 5
+  }
+  const fetchNotesFromDB = async() => {
+    const result = await getNotes()
+    console.log('in fetchnotes',result)
+    setNotes(result)
+    console.log('in fetchnotes',typeof notes)
   }
 
   return (
     <Router>
       <div>
         <Link style={padding} to ='/'>home</Link>
-        <Link style={padding} to ='/notes'>notes</Link>
+        <Link style={padding} to ='/notes' onClick={fetchNotesFromDB}>notes</Link>
         <Link style={padding} to ='/users'>users</Link>
       </div>
       <Routes>
-        <Route path = '/notes' element = {<Notes />}/>
+        <Route path = '/notes' element = {<Notes notes={notes}/>}/>
         <Route path = '/users' element = {<Users />}/>
         <Route path = '/' element = {<Home />}/>
       </Routes>
--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,6 +1,15 @@
-const Notes = () =>{
+const Notes = ({notes}) =>{
+  console.log('in notes',notes)
   return(
-  <div> <h2>Notes</h2> </div>
+  <div>
+    <h2>Notes</h2>
+    <ul>
+      {notes.map(note =>{
+        console.log(note.content)
+        {<li> {note.content} </li>}
+      })}
+    </ul>
+  </div>
 )
 }
 
